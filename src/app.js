9bc4d484b0983bc24ee2448ea62eeb26f86a33e1
Alisher <info@regname.uz>
2023-11-25 11:34:16 +0500

    fixed render word raz raza

--- src/app.js
+++ src/app.js
@@ -9,36 +9,44 @@ import './styles.css';
  */
 function App({store}) {
 
-  const list = store.getState().list;
-    console.log(list)
-  return (
-    <div className='App'>
-      <div className='App-head'>
-        <h1>Приложение на чистом JS</h1>
-      </div>
-      <div className='App-controls'>
-        <button onClick={() => store.addItem()}>Добавить</button>
-      </div>
-      <div className='App-center'>
-        <div className='List'>{
-          list.map(item =>
-            <div key={item.code} className='List-item'>
-              <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
-                <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}{item.clickCount === 0 ? "" : ` | Выделяли ${item.clickCount}`}</div>
-                <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
-                    Удалить
-                  </button>
-                </div>
-              </div>
+    const list = store.getState().list;
+    const getCount = (count) => {
+        return count === 0 ? "" : ` | Выделяли ${count} ${getManyOrSingle(count)}`
+    }
+    const getManyOrSingle = (count) => {
+        if (count === 12 || count === 14) return "раз"
+        let res = count % 10;
+        if (res >= 2 && res <= 4) return "раза"
+        else return "раз"
+    }
+    return (
+        <div className='App'>
+            <div className='App-head'>
+                <h1>Приложение на чистом JS</h1>
             </div>
-          )}
+            <div className='App-controls'>
+                <button onClick={() => store.addItem()}>Добавить</button>
+            </div>
+            <div className='App-center'>
+                <div className='List'>{
+                    list.map(item =>
+                        <div key={item.code} className='List-item'>
+                            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
+                                 onClick={() => store.selectItem(item.code)}>
+                                <div className='Item-code'>{item.code}</div>
+                                <div className='Item-title'>{item.title}{getCount(item.clickCount)}</div>
+                                <div className='Item-actions'>
+                                    <button onClick={() => store.deleteItem(item.code)}>
+                                        Удалить
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
